c3cba07f0064d7c32e50fa1d38cb811aac17e487
Eugene Vdovenko <zhenbok@gmail.com>
2022-04-12 13:55:31 +0300

    147 Working on a More Complex Reducer Logic

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,18 +11,20 @@ const Cart = (props) => {
 
   const cartItemAddHandler = (item) => {};
 
-  const cartItems = cartCtx.items.map((item) => (
+  const cartItems = (
     <ul className={classes["cart-items"]}>
-      <CartItem
-        key={item.id}
-        name={item.name}
-        amount={item.amount}
-        price={item.price}
-        onRemove={cartItemRemoveHandler.bind(null, item.id)}
-        onAdd={cartItemAddHandler.bind(null, item)}
-      />
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
+      ))}
     </ul>
-  ));
+  );
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
   return (
